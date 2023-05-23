8f2f74f64ff1cba9c13dbf13e2f210604042bfa3
shahadatwebd <shahadatwebd@gmail.com>
2023-05-23 11:21:21 +0600

    main

---
+++ 3_variables/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$welcome_text = 'My Name is shahadat';
+echo $welcome_text . '<br/>';
+
+$bangladesh = 'Hello Bangladesh';
+echo $bangladesh . '<br/>';
+
+$addres = 'Chapainabwang';
+echo $addres; 
+
+?>
